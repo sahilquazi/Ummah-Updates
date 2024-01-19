fa7de60f87ae934966ad018239552d914d0d6d65
Sahil Quazi <sahil.quazi@gmail.com>
2024-01-18 20:21:16 -0500

    Initial Commit

---
+++ Ummah Updates/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Ummah Updates
+//
+//  Created by Sahil Quazi on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Ummah Updates!")
+        }
+        .padding()
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ummah Updates/Ummah_UpdatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ummah_UpdatesApp.swift
+//  Ummah Updates
+//
+//  Created by Sahil Quazi on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Ummah_UpdatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
